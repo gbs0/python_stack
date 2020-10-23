6513753082402516361d4e6fc536555ff7a51bd3
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-10-23 09:49:55 -0300

    upadte ruby example

--- katas/forecasting.rb
+++ katas/forecasting.rb
@@ -40,9 +40,10 @@ def calc_lifetime(month_lifetime, target_market_share, quantity, sell_throught, 
   # Calcular Sell-Thru
   st = calc_st(target_market_share, sell_throught)
   st_per_month = st / st_months
+  units = quantity
   
   month_lifetime.times do
-  	row.push({ month: months[i].to_s, sales: st_per_month.to_s, units: 10 })
+  	row.push({ month: months[i].to_s, sales: st_per_month.to_s, units:  })
   end
 
   print("#{row[:month]}               #{row[:sales]}                       #{row[:units]}")
